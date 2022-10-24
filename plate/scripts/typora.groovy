15f7faea4c8e7f82790e842c53ddfeb582dc8af3
Junlong Zhang <junlongzzz@qq.com>
2022-10-24 23:09:57 +0800

    Add app typora [windows,linux,macos] (repush)

---
+++ plate/scripts/typora.groovy
@@ -0,0 +1,25 @@
+static def checkUpdate(version, platform, args) {
+    // 目前所有平台版本一致
+    def response = 'https://typoraio.cn/releases/stable.html'.toURL().text
+    def pattern = switch (platform) {
+        case 'windows' -> 'windows/typora-setup-x64-([\\d.]+).exe'
+        case 'linux' -> 'linux/Typora-linux-x64-([\\d.]+).tar.gz'
+        case 'mac' -> 'mac/Typora-([\\d.]+).dmg'
+        default -> null
+    }
+    def matcher = response =~ pattern
+    if (!matcher.find()) {
+        return null
+    }
+    version = matcher[0][1] as String
+
+    return [
+            'version': version,
+            'url'    : switch (platform) {
+                case 'windows' -> "https://download.typoraio.cn/windows/typora-setup-x64-${version}.exe".toString()
+                case 'linux' -> "https://download.typoraio.cn/linux/Typora-linux-x64-${version}.tar.gz".toString()
+                case 'mac' -> "https://download.typoraio.cn/mac/Typora-${version}.dmg".toString()
+                default -> null
+            }
+    ]
+}
