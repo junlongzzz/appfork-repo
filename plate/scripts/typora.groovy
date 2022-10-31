ef2ac69a9305e95be5733d71971d8e05fa71b671
Junlong Zhang <junlongzzz@qq.com>
2022-10-31 23:07:21 +0800

    update workflow update app typora script

--- plate/scripts/typora.groovy
+++ plate/scripts/typora.groovy
@@ -1,6 +1,7 @@
 static def checkUpdate(version, platform, args) {
     // 目前所有平台版本一致
-    def response = 'https://typoraio.cn/releases/stable.html'.toURL().text
+//    def response = 'https://typoraio.cn/releases/stable.html'.toURL().text
+    def response = 'https://typora.io/releases/stable'.toURL().text
     def pattern = switch (platform) {
         case 'windows' -> 'windows/typora-setup-x64-([\\d.]+).exe'
         case 'linux' -> 'linux/Typora-linux-x64-([\\d.]+).tar.gz'
