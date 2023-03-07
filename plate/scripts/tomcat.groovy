296802a9fc73560f132f0e83ef62e88e278bd140
Junlong Zhang <junlongzzz@qq.com>
2023-03-07 17:50:01 +0800

    Add app: Apache Tomcat 8,9,10

---
+++ plate/scripts/tomcat.groovy
@@ -0,0 +1,25 @@
+static def checkUpdate(version, platform, args) {
+    if (!args) {
+        return null
+    }
+    def baseUrl = 'https://dlcdn.apache.org'
+
+    def response = "${baseUrl}/tomcat/tomcat-${args.ver}/?C=M;O=D".toURL().text
+    def matcher = response =~ 'v([\\d.]+)/'
+    if (!matcher.find()) {
+        return null
+    }
+    version = matcher[0][1] as String
+
+    def url = [:]
+    ["${baseUrl}/tomcat/tomcat-${args.ver}/v${version}/bin/apache-tomcat-${version}.exe" as String,
+     "${baseUrl}/tomcat/tomcat-${args.ver}/v${version}/bin/apache-tomcat-${version}-windows-x64.zip" as String,
+     "${baseUrl}/tomcat/tomcat-${args.ver}/v${version}/bin/apache-tomcat-${version}-windows-x86.zip" as String].each {
+        url[it.substring(it.lastIndexOf('/') + 1)] = it
+    }
+
+    return [
+            'version': version,
+            'url'    : url
+    ]
+}
