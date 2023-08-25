cc53313e06861db2aa679b3153dbb30617a8e974
Junlong Zhang <junlongzzz@qq.com>
2023-08-25 12:51:18 +0800

    fix: bdnetdisk script request connect timeout

--- plate/scripts/bdnetdisk.groovy
+++ plate/scripts/bdnetdisk.groovy
@@ -1,7 +1,25 @@
 import groovy.json.JsonSlurper
 
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.time.Duration
+
 static def checkUpdate(version, platform, args) {
-    def response = "https://pan.baidu.com/disk/cmsdata?clienttype=0&app_id=250528&t=${System.currentTimeMillis()}&do=client".toURL().text
+//    def response = "https://pan.baidu.com/disk/cmsdata?clienttype=0&app_id=250528&t=${System.currentTimeMillis()}&do=client".toURL().text
+
+    def ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.54'
+
+    def httpClient = HttpClient.newBuilder()
+            .connectTimeout(Duration.ofMillis(30000))
+            .build()
+    def response = httpClient.send(
+            HttpRequest.newBuilder("https://pan.baidu.com/disk/cmsdata?clienttype=0&app_id=250528&t=${System.currentTimeMillis()}&do=client".toURI())
+                    .header("user-agent", ua)
+                    .build(),
+            HttpResponse.BodyHandlers.ofString()
+    ).body()
+
     def jsonData = new JsonSlurper().parseText(response)
     if (jsonData.errorno != 0) {
         return null
