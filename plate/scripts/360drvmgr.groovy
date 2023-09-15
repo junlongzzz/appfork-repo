ea0a951428f811a6c32893904a41483e1a8aea63
Junlong Zhang <junlongzzz@qq.com>
2023-09-15 17:06:53 +0800

    fix: 360驱动大师、360压缩检测更新脚本加入http请求超时限制

--- plate/scripts/360drvmgr.groovy
+++ plate/scripts/360drvmgr.groovy
@@ -1,3 +1,8 @@
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.time.Duration
+
 static def checkUpdate(version, platform, args) {
     // 在 2.0.0.1740 版本的时候，返回的json内的 details 的值是数组但是多了一个逗号，导致无法进行json解析
 //    def matcher = response =~ 'jsonpCallback\\(([\\s\\S]*)\\)'
@@ -7,7 +12,14 @@ static def checkUpdate(version, platform, args) {
 //    def jsonData = new JsonSlurper().parseText("{\"data\":${matcher[0][1]}}")
 //    return jsonData.data[0].version
 
-    def response = "https://weishi.360.cn/qudongdashi/updateData.json?callback=jsonpCallback&_=${System.currentTimeMillis()}".toURL().text
+    def httpClient = HttpClient.newBuilder()
+            .connectTimeout(Duration.ofMillis(30000))
+            .build()
+    def response = httpClient.send(
+            HttpRequest.newBuilder("https://weishi.360.cn/qudongdashi/updateData.json?callback=jsonpCallback&_=${System.currentTimeMillis()}".toURI())
+                    .build(),
+            HttpResponse.BodyHandlers.ofString()
+    ).body()
     // 正则匹配任意字符
     def matcher = response =~ '"version": "([\\d.]+)"'
     if (!matcher.find()) {
--- plate/scripts/360zip.groovy
+++ plate/scripts/360zip.groovy
@@ -1,30 +1,33 @@
 import groovy.json.JsonSlurper
 
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.time.Duration
+
 static def checkUpdate(version, platform, args) {
-    def response = 'https://yasuo.360.cn/update/versioninfo.json'.toURL().text
+    def httpClient = HttpClient.newBuilder()
+            .connectTimeout(Duration.ofMillis(30000))
+            .build()
+    def response = httpClient.send(
+            HttpRequest.newBuilder('https://yasuo.360.cn/update/versioninfo.json'.toURI())
+                    .build(),
+            HttpResponse.BodyHandlers.ofString()
+    ).body()
     def jsonData = new JsonSlurper().parseText(response)
     if (jsonData.errno != 0) {
         return null
     }
 
-    def versionNew = null
-    def url = null
-
-    switch (platform) {
-        case 'windows':
-            versionNew = jsonData.data.winCode
-            url = jsonData.data.winDownloadUrl
-            break
-        case 'mac':
-            versionNew = jsonData.data.macCode
-            url = jsonData.data.macDownloadUrl
-            break
-        default:
-            return null
+    return switch (platform) {
+        case 'windows' -> [
+                'version': jsonData.data.winCode,
+                'url'    : jsonData.data.winDownloadUrl
+        ]
+        case 'mac' -> [
+                'version': jsonData.data.macCode,
+                'url'    : jsonData.data.macDownloadUrl
+        ]
+        default -> null
     }
-
-    return [
-            'version': versionNew,
-            'url'    : url
-    ]
 }
