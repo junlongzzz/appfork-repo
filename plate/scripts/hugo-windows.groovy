f838ce04e6b1d4e17b0959eecaf942e081f95fe3
Junlong Zhang <junlongzzz@qq.com>
2023-10-06 22:48:21 +0800

    Hugo: Add app [windows]

---
+++ plate/scripts/hugo-windows.groovy
@@ -0,0 +1,22 @@
+import groovy.json.JsonSlurper
+
+static def checkUpdate(version, platform, args) {
+    def response = 'https://api.github.com/repos/gohugoio/hugo/releases/latest'.toURL().text
+    def result = new JsonSlurper().parseText(response)
+
+    def tagName = result.tag_name as String
+    version = tagName.replaceFirst('[vV]', '')
+
+    def url = [:]
+    for (asset in result.assets) {
+        def name = asset.name as String
+        if (name.containsIgnoreCase('_windows')) {
+            url[name] = asset.browser_download_url
+        }
+    }
+
+    return [
+            version: version,
+            url    : url
+    ]
+}
