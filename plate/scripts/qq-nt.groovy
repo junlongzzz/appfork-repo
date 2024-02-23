c5e059b354f1c9ee4e05d1ef6c4fd04e084b2fd1
Junlong Zhang <junlongzzz@qq.com>
2024-02-23 15:35:00 +0800

    QQ Linux: Add version 3.2.5 QQ Windows: Fix version check QQ Android: Deprecated

--- plate/scripts/qq-nt.groovy
+++ plate/scripts/qq-nt.groovy
@@ -1,20 +1,36 @@
 import groovy.json.JsonSlurper
 
 static def checkUpdate(version, platform, args) {
-    def response = 'https://cdn-go.cn/qq-web/im.qq.com_new/latest/rainbow/windowsDownloadUrl.js'.toURL().text
+    def checkUrl = switch (platform) {
+        case 'windows' -> 'https://cdn-go.cn/qq-web/im.qq.com_new/latest/rainbow/windowsDownloadUrl.js'
+        case 'linux' -> 'https://cdn-go.cn/qq-web/im.qq.com_new/latest/rainbow/linuxQQDownload.js'
+        default -> null
+    }
+    if (checkUrl == null) {
+        return null
+    }
+    def response = checkUrl.toURL().text
     def matcher = response.replace(' ', '') =~ 'params=(\\{.*})'
     if (!matcher.find()) {
         return null
     }
     def object = new JsonSlurper().parseText(matcher.group(1))
-    def url = object.ntDownloadX64Url
-    def versionMatcher = url =~ '/QQ([\\d.]+)_x64.exe'
-    if (!versionMatcher.find()) {
-        return null
+
+    def url = null
+    if (platform == 'linux') {
+        version = object.version
+        url = object.x64DownloadUrl
+    } else if (platform == 'windows') {
+        url = args.nt ? object.ntDownloadX64Url : object.downloadUrl
+        def versionMatcher = url =~ (args.nt ? '/QQ([\\d.]+)_x64.exe' : '/QQ([\\d.]+).exe')
+        if (!versionMatcher.find()) {
+            return null
+        }
+        version = versionMatcher.group(1)
     }
 
     return [
-            version: versionMatcher.group(1),
+            version: version,
             url    : url
     ]
 }
