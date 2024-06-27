239760698b7d983b77c68c54cde3812421939106
Junlong Zhang <junlongzzz@qq.com>
2024-06-27 11:42:24 +0800

    refactor: qq/qq-nt check update script

--- plate/scripts/qq-nt.groovy
+++ plate/scripts/qq-nt.groovy
@@ -5,19 +5,33 @@ static def checkUpdate(manifest, args) {
     def platform = manifest.platform as String
     def isNt = args ? (args.nt as Boolean) : false
 
-    def checkUrl = switch (platform) {
-        case 'windows' -> 'https://cdn-go.cn/qq-web/im.qq.com_new/latest/rainbow/windowsDownloadUrl.js'
-        case 'linux' -> 'https://cdn-go.cn/qq-web/im.qq.com_new/latest/rainbow/linuxQQDownload.js'
-        default -> null
+    def timeout = 60000
+    def headers = [
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+            'User-Agent'     : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' +
+                    'Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'
+    ]
+
+    // 先获取网页，从网页内获取版本信息请求地址
+    def html = HttpUtil.createGet(manifest.homepage as String, true).timeout(timeout).headerMap(headers, true)
+            .execute().body()
+    def urlMatcher = ((html == null || html.isEmpty()) ? '' : html).replace(' ', '') =~ 'varrainbowConfigUrl="(?<url>.*)";'
+    def checkUrl
+    if (urlMatcher.find()) {
+        checkUrl = urlMatcher.group('url')
+    } else {
+        // 网页内未找到，使用默认地址
+        checkUrl = switch (platform) {
+            case 'windows' -> 'https://cdn-go.cn/qq-web/im.qq.com_new/latest/rainbow/windowsDownloadUrl.js'
+            case 'linux' -> 'https://cdn-go.cn/qq-web/im.qq.com_new/latest/rainbow/linuxQQDownload.js'
+            default -> null
+        }
     }
     if (checkUrl == null) {
         return null
     }
-    def response = HttpUtil.createGet(checkUrl, true)
-            .timeout(60000)
-            .header('Accept-Language', 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6')
-            .header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' +
-                    'Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0')
+
+    def response = HttpUtil.createGet(checkUrl, true).timeout(timeout).headerMap(headers, true)
             .execute().body()
     if (response == null || response.isEmpty()) {
         return null
