8b486f7703870f329c2a948cd1bcb926c6f0775d
Junlong Zhang <junlongzzz@qq.com>
2024-07-23 11:10:44 +0800

    fix(docker-desktop): Fix script version regex chore: Upgrade to SpringBoot 3.3.2

--- plate/scripts/docker-desktop.groovy
+++ plate/scripts/docker-desktop.groovy
@@ -6,22 +6,25 @@ static def checkUpdate(manifest, args) {
     }
 
     def response = 'https://docs.docker.com/desktop/release-notes/'.toURL().text
-    def matcher = response =~ '/docker-desktop-([\\d.]+)-'
+    def matcher = response =~ '>([\\d.]+)<'
     if (!matcher.find()) {
         return null
     }
     def version = matcher.group(1)
 
     def url = switch (platform) {
-        case 'windows' -> 'https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe'
+        case 'windows' -> [
+                'Amd64': 'https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe',
+                'Arm64': 'https://desktop.docker.com/win/main/arm64/Docker%20Desktop%20Installer.exe'
+        ]
         case 'mac' -> [
                 'Mac with Intel chip': 'https://desktop.docker.com/mac/main/amd64/Docker.dmg',
                 'Mac with Apple chip': 'https://desktop.docker.com/mac/main/arm64/Docker.dmg'
         ]
         case 'linux' -> [
-                'Linux DEB' : "https://desktop.docker.com/linux/main/amd64/docker-desktop-${version}-amd64.deb".toString(),
-                'Linux RPM' : "https://desktop.docker.com/linux/main/amd64/docker-desktop-${version}-x86_64.rpm".toString(),
-                'Linux Arch': "https://desktop.docker.com/linux/main/amd64/docker-desktop-${version}-x86_64.pkg.tar.zst".toString()
+                'Linux DEB' : "https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb".toString(),
+                'Linux RPM' : "https://desktop.docker.com/linux/main/amd64/docker-desktop-x86_64.rpm".toString(),
+                'Linux Arch': "https://desktop.docker.com/linux/main/amd64/docker-desktop-x86_64.pkg.tar.zst".toString()
         ]
         default -> null
     }
