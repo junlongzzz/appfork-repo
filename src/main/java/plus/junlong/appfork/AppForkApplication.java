a3e0a2a39bb8f2d383d1a5fb8c9c3ee39c27ea13
Junlong Zhang <junlongzzz@qq.com>
2024-10-15 15:02:27 +0800

    chore: 设置默认时区 chore: 更新部分清单文件属性

--- src/main/java/plus/junlong/appfork/AppForkApplication.java
+++ src/main/java/plus/junlong/appfork/AppForkApplication.java
@@ -15,6 +15,10 @@ public class AppForkApplication implements CommandLineRunner {
     private final Updater updater;
 
     public static void main(String[] args) {
+        if (!System.getProperties().containsKey("user.timezone")) {
+            // 如果时区属性为空则设置默认时区
+            System.setProperty("user.timezone", "Asia/Shanghai");
+        }
         SpringApplication.run(AppForkApplication.class, args);
     }
 
