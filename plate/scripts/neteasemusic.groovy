c4d1e695e00af52970b28e67f32593f577e8a488
Junlong Zhang <junlongzzz@qq.com>
2024-10-24 10:35:02 +0800

    NeteaseMusic: Update script

--- plate/scripts/neteasemusic.groovy
+++ plate/scripts/neteasemusic.groovy
@@ -1,50 +1,35 @@
+import groovy.json.JsonSlurper
 import plus.junlong.appfork.ScriptVars
 
-import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
 static def checkUpdate(manifest, args) {
-    //    def html = "https://pc.qq.com/detail/7/detail_11167.html".toURL().text
-//    def matcher = html =~ "/cloudmusicsetup([\\d.]+).exe"
-//    if (!matcher.find()) {
-//        return null
-//    }
-//    return matcher[0][1]
-//    def response = "https://raw.fastgit.org/chawyehsu/dorado/master/bucket/neteasemusic.json".toURL().text
-//    def response = "https://cdn.jsdelivr.net/gh/chawyehsu/dorado@master/bucket/neteasemusic.json".toURL().text
-//    def response = "https://gitee.com/chawyehsu/dorado/raw/master/bucket/neteasemusic.json".toURL().text
-//    def response = "https://ghproxy.com/https://raw.githubusercontent.com/chawyehsu/dorado/master/bucket/neteasemusic.json".toURL().text
-//    return new JsonSlurper().parseText(response).version
-
-//    def headers = OkHttpUtil.head('https://music.163.com/api/pc/package/download/latest')
-//    if (headers == null) {
-//        return null
-//    }
-
-//    def connection = (HttpURLConnection) 'https://music.163.com/api/pc/package/download/latest'.toURL().openConnection()
-//    connection.setRequestMethod('HEAD')
-//    connection.setConnectTimeout(10000)
-//    connection.setInstanceFollowRedirects(false)
-//    def location = connection.getHeaderField('Location')
-
-    def url = 'https://music.163.com/api/pc/package/download/latest'
-    def httpClient = ScriptVars.newHttpClientBuilder().followRedirects(HttpClient.Redirect.NEVER).build()
     def request = HttpRequest.newBuilder()
-            .uri(URI.create(url))
+            .uri('https://music.163.com/api/appcustomconfig/get?key=web-pc-beta-download-links'.toURI())
             .header('User-Agent', ScriptVars.USER_AGENT)
-            .method('HEAD', HttpRequest.BodyPublishers.noBody())
+            .GET()
             .build()
-    def response = httpClient.send(request, HttpResponse.BodyHandlers.discarding())
-    def location = response.headers().firstValue('location').orElse(null)
-    def matcher = location =~ '/*_([\\d.]+)'
+    def response = ScriptVars.HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString()).body()
+    def matcher = response =~ '/*_([\\d.]+)'
     if (!matcher.find()) {
+        return null
+    }
+    def respJson = new JsonSlurper().parseText(response)
+    if (respJson.code != 200 || !respJson['data']) {
+        return null
+    }
+    def links = respJson['data']['web-pc-beta-download-links']
+    if (!links) {
         return null
     }
 
     return [
             version: matcher.group(1),
-            url    : url
+            url    : [
+                    '64bit': links['pcPackage64'],
+                    '32bit': links['pcPackage32']
+            ]
     ]
 }
 
