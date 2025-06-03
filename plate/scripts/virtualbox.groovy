3d66735fcda5f169308519efd84e193f904a319f
Junlong Zhang <junlongzzz@qq.com>
2025-06-03 11:00:22 +0800

    refactor: 重构 Virtualbox 更新检测脚本

--- plate/scripts/virtualbox.groovy
+++ plate/scripts/virtualbox.groovy
@@ -1,15 +1,23 @@
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
+import plus.junlong.appfork.ScriptVars
+
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 
 static def checkUpdate(manifest, args) {
-    def document = Jsoup.parse('https://www.virtualbox.org/wiki/Downloads'.toURL(), 60000)
+    def request = HttpRequest.newBuilder('https://www.virtualbox.org/wiki/Downloads'.toURI())
+            .header('User-Agent', ScriptVars.USER_AGENT).GET().build()
+    def response = ScriptVars.HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString())
+    def document = Jsoup.parse(response.body())
     if (!document) {
         return null
     }
 
     def version = null
     def url = []
-    for (Element element : document.select('ul a.ext-link[href]')) {
+    def elements = document.select('ul a.ext-link[href], .license-button[href]')
+    for (Element element : elements) {
         def href = element.attr('href')
         if (href.endsWith('.exe')) {
             def matcher = href =~ '/(?<version>[\\d.]+)/'
